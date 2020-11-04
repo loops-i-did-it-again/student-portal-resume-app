a481ca1ed4bb9e81ddb6d592321200c535c6a472
winstonbarbe <winston.barbe@gmail.com>
2020-11-04 12:34:08 -0500

    Creates login view

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,15 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+  { path: "/", name: "home", component: Home },
+  { path: "/about", name: "about", component: About },
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login }
+];

f6fc0a19450516dc48c4baa5d57f9d582c904eea
merrow-ragan <seragan3@gmail.com>
2020-11-05 14:11:58 -0500

    adds experiences edit page

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,22 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
-import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
+import ExperiencesEdit from "../views/ExperiencesEdit"
 
 Vue.use(VueRouter);
 
 const routes = [
   { path: "/", name: "home", component: Home },
   { path: "/about", name: "about", component: About },
-  { path: "/signup", name: "signup", component: Signup },
-  { path: "/login", name: "login", component: Login }
+  { path: "/login", name: "login", component: Login },
+  { path: "/experiences/:id/edit", name: "experiences-edit", component: ExperiencesEdit}
 ];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router

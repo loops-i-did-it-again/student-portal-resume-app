998b0d4009b603a2fd10713f923c813344c9745e
seldenleonard <selden222@gmail.com>
2020-11-05 09:35:19 -0800

    adds ExperiencesNew, EducationsNew, and SkillsNew actions

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import EducationsNew from '../views/EducationsNew.vue'
+import ExperiencesNew from '../views/ExperiencesNew.vue'
+import SkillsNew from '../views/SkillsNew.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +20,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/educations/new',
+    name: 'educations-new',
+    component: EducationsNew,
+  },
+  {
+    path: '/experiences/new',
+    name: 'experiences-new',
+    component: ExperiencesNew,
+  },
+  {
+    path: '/skills/new',
+    name: 'skills-new',
+    component: SkillsNew,
+  },
 ]
 
 const router = new VueRouter({
